a6bcf8d77c7da23f76f0b4d12ddd32a0ad8d781c
Amukozoberit <mwasheb@gmail.com>
2021-06-19 16:45:06 +0300

    make the what-to-do toggle

--- js/script.js
+++ js/script.js
@@ -1,42 +1,65 @@
-let designText = "Design content";
-let developText = "Development content";
-let productText = "Product Text";
+// let designText = document.getElementById("designText").innerHTML;
+// // console.log(designText.innerHTML);
+// let developText = document.querySelector("#developText").innerHTML;
+// let productText = document.querySelector("#productText").innerHTML;
 
 
-//acces the divs
-const dev = document.querySelector("#develop");
-const prod = document.getElementById("product");
-const des = document.getElementById("design");
-let devLoad = function() {
-        dev.innerHTML = developText;
-    }
-    // creating functions for access
+// //acces the divs
+// let dev = document.getElementById("develop");
+// let prod = document.getElementById("product");
+// let des = document.getElementById("design");
 
-let prodLoad = function() {
-    prod.innerHTML = productText;
-}
+// // creating functions for access
 
-function desLoad() {
-    des.innerHTML = designText;
-}
-
-
-
-//call the events
-des.addEventListener('click', desLoad);
-prod.addEventListener('click', prodLoad);
-dev.addEventListener('click', devLoad)
-
-
-// function allclicks(item, func) {
-//     item.addEventListener('click', func);
+// let prodLoad = function() {
+//     const prod = document.getElementById('product');
+//     prod.innerHTML = productText;
 // }
-// allclicks(des, )
+// let devLoad = function() {
+//     const dev = document.getElementById('develop');
+//     dev.innerHTML = developText;
+// }
+
+// // const dess = document.getElementById("design");
+// // alert(dess.innerHTML);
+// // let desLoad = function() {
+// //     alert(dess);
+// //     if (des.innerHTML === designText) {
+
+// //         des.innerHTML = dess.innerHTML;
+// //         // alert(des.innerHTML)
+// //     } else {
+
+// //         des.innerHTML = designText;
+// //     }
+// // }
+
+
+// // //call the events
+// //des.addEventListener('click', desLoad); //designText.addEventListener('click', designLoad);
+// // //des.innerHTML.addEventListener('click', alert("new "));
+// prod.addEventListener('click', prodLoad);
+// dev.addEventListener('click', devLoad)
+
+
+
 
 $(document).ready(function() {
     $(".social-text").hide();
     $(".social").click(function() {
         $(".social-text").hide();
         $("#div" + $(this).attr('target')).show();
-    })
+    });
+    $(".designtoggle").click(function() {
+        $(this).find('.card-a').toggle();
+    });
+    $(".devtoggle").click(function() {
+        $(this).find('.card-a').toggle();
+    });
+    $(".prodtoggle").click(function() {
+        $(this).find('.card-a').toggle();
+    });
+
+
+
 });
