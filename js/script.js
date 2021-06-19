aceee17062d10814c8b96d02b3572c4518ae3a86
Amukozoberit <mwasheb@gmail.com>
2021-06-19 11:01:44 +0300

    initialized the js functions and accesed the html function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
-    // $(".social").click(function() {
-    //     // alert("facebook");
-    //     $(".social-text").hide();
-    //     $("#div1").show();
+let designText = "Design content";
+let developText = "Development content";
+let productText = "Product Text";
 
-    // });
+
+
+const des = document.querySelector("#design");
+// des.innerHTML = designText;
+
+const prod = document.getElementById("prod");
+// prod.innerHTML = productText;
+
+const dev = document.querySelector("#dev");
+// dev.innerHTML = developText;
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
     $(".social-text").hide();
     $(".social").click(function() {
         $(".social-text").hide();
