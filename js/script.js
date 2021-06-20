5fd6046fa1506f835906b5b6bffe919976b30b4d
Amukozoberit <mwasheb@gmail.com>
2021-06-20 15:26:24 +0300

    added updates

--- js/script.js
+++ js/script.js
@@ -60,6 +60,68 @@ $(document).ready(function() {
         $(this).find('.card-a').toggle();
     });
 
+    $("#work1").mouseover(function() {
+        $('#overlay').show();
+
+    }).mouseout(function() {
+        $('#overlay').hide();
+    });
+    $("#work2").mouseover(function() {
+        $('#overlay1').show();
+
+    }).mouseout(function() {
+        $('#overlay1').hide();
+    });
+
+    $("#work3").mouseover(function() {
+        $('#overlay2').show();
+
+    }).mouseout(function() {
+        $('#overlay2').hide();
+    });
+    $("#work4").mouseover(function() {
+        $('#overlay3').show();
+
+    }).mouseout(function() {
+        $('#overlay3').hide();
+    });
+    $("#work5").mouseover(function() {
+        $('#overlay4').show();
+
+    }).mouseout(function() {
+        $('#overlay4').hide();
+    });
+    $("#work6").mouseover(function() {
+        $('#overlay5').show();
+
+    }).mouseout(function() {
+        $('#overlay5').hide();
+    });
+    $("#work7").mouseover(function() {
+        $('#overlay6').show();
+
+    }).mouseout(function() {
+        $('#overlay6').hide();
+    });
+    $("#work8").mouseover(function() {
+        $('#overlay7').show();
+
+    }).mouseout(function() {
+        $('#overlay7').hide();
+    });
+
+
+
 
 
 });
+// $(document).ready(function() {
+//     $("#work2").mouseover(function() {
+//         $('#overlay1').show();
+
+//         // alert("a");
+//     }).mouseout(function() {
+//         $('#overlay1').hide();
+//     });
+
+// });
